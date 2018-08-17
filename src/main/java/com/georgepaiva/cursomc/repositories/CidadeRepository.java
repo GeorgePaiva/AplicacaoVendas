666161a5f9d827711a550078dc7a6726322b6873
George Paiva <george.paiva15@gmail.com>
2018-08-17 16:41:34 -0300

    Estado e cidade

---
+++ src/main/java/com/georgepaiva/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.georgepaiva.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.georgepaiva.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+
+}
