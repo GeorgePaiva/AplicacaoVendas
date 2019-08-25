b4a62f6ad572340d6ac53b898f3747d689d74eec
George Paiva <george.paiva15@gmail.com>
2019-08-25 17:40:55 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/georgepaiva/cursomc/domain/Pagamento.java
+++ src/main/java/com/georgepaiva/cursomc/domain/Pagamento.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.georgepaiva.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -23,7 +23,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer estado;
 
 	// MAPEAMENTO DO ATRIBUTO PEDIDO
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
